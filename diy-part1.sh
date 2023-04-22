c0557c93b363793b4f41a106c81c52cd1cc44503
Fonghang <30502871+hysbeta@users.noreply.github.com>
2023-04-22 13:48:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,14 +18,10 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # Add customize apps
-# apt update -y
-# apt full-upgrade -y
-# apt install -y ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential bzip2 ccache cmake cpio curl device-tree-compiler fastjar flex gawk gettext gcc-multilib g++-multilib git gperf haveged help2man intltool libc6-dev-i386 libelf-dev libglib2.0-dev libgmp3-dev libltdl-dev libmpc-dev libmpfr-dev libncurses5-dev libncursesw5-dev libreadline-dev libssl-dev libtool lrzsz mkisofs msmtp nano ninja-build p7zip p7zip-full patch pkgconf python2.7 python3 python3-pyelftools libpython3-dev qemu-utils rsync scons squashfs-tools subversion swig texinfo uglifyjs upx-ucl unzip vim wget xmlto xxd zlib1g-dev
 # git clone https://github.com/coolsnowwolf/lede
 # cd lede
 git clone --depth 1 --branch master --single-branch https://github.com/vernesong/OpenClash package/OpenClash
 git clone --depth 1 --branch master --single-branch https://github.com/kuoruan/openwrt-frp package/frp
-git clone --depth 1 --branch master --single-branch https://github.com/kuoruan/openwrt-v2ray.git package/v2ray-core
 #git clone --depth 1 --branch master --single-branch https://github.com/yichya/openwrt-xray package/openwrt-xray
 #git clone --depth 1 --branch master --single-branch https://github.com/yichya/openwrt-xray-geodata-cut package/openwrt-xray-geodata-cut
 #git clone --depth 1 --branch master --single-branch https://github.com/yichya/luci-app-xray package/luci-app-xray
