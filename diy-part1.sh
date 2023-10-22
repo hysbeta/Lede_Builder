7735125b2b6c32905760690d2dc19439c689b933
Fonghang <30502871+hysbeta@users.noreply.github.com>
2023-10-22 10:44:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -25,10 +25,6 @@
 # apt install -y ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential bzip2 ccache cmake cpio curl device-tree-compiler fastjar flex gawk gettext gcc-multilib g++-multilib git gperf haveged help2man intltool libc6-dev-i386 libelf-dev libglib2.0-dev libgmp3-dev libltdl-dev libmpc-dev libmpfr-dev libncurses5-dev libncursesw5-dev libreadline-dev libssl-dev libtool lrzsz mkisofs msmtp nano ninja-build p7zip p7zip-full patch pkgconf python2.7 python3 python3-pyelftools libpython3-dev qemu-utils rsync scons squashfs-tools subversion swig texinfo uglifyjs upx-ucl unzip vim wget xmlto xxd zlib1g-dev python3-setuptools
 git clone --depth 1 --branch master --single-branch https://github.com/vernesong/OpenClash package/OpenClash
 git clone --depth 1 --branch master --single-branch https://github.com/kuoruan/openwrt-frp package/frp
-find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
-find ./ | grep Makefile | grep mosdns | xargs rm -f
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 #git clone --depth 1 --branch master --single-branch https://github.com/yichya/openwrt-xray package/openwrt-xray
 #git clone --depth 1 --branch master --single-branch https://github.com/yichya/openwrt-xray-geodata-cut package/openwrt-xray-geodata-cut
 #git clone --depth 1 --branch master --single-branch https://github.com/yichya/luci-app-xray package/luci-app-xray
